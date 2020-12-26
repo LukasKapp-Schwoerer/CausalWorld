11d4f07c5bcdd6db56c19e61ae1579454856dd0e
Batuhan Tomekce <tbatuhan@student.ethz.ch>
2020-12-26 03:50:03 +0300

    Code update, tslearn implementation

--- causal_world/dynamics_model/simulator_model.py
+++ causal_world/dynamics_model/simulator_model.py
@@ -5,6 +5,9 @@ from sdtw.barycenter import sdtw_barycenter
 from sdtw.distance import SquaredEuclidean
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances
+from sklearn.preprocessing import StandardScaler
+from tslearn.metrics import soft_dtw # https://github.com/tslearn-team/tslearn
+from tslearn.clustering import TimeSeriesKMeans
 
 class SimulatorModel(object):
 
@@ -59,7 +62,14 @@ class SimulatorModel(object):
 
 class ExperimentingSimulatorModel(object):
 
-    def __init__(self, _make_env_func, parallel_agents, num_environments, use_z_only=False):
+    def __init__(self, 
+                _make_env_func, 
+                parallel_agents, 
+                num_environments, 
+                use_z_only=False, 
+                metric='softdtw', 
+                reward='causal_curiosity',
+                num_clusters=2):
         """
         This class instantiates a dynamics model based on the pybullet simulator
         (i.e: simulates exactly the result of the actions), it can be used
@@ -71,6 +81,11 @@ class ExperimentingSimulatorModel(object):
                                       to evaluate the actions.
         :param num_environments: (int) number of different environments to siumulate
                                       to evaluate the actions.
+        :param metric: (string): the distance or discrepency measure that can be used
+                                      for kmeans clustering. It can be "softdtw" for 
+                                      Soft-Dynamic Time Warping or "euclidian". 
+        :param reward: (string): either "causal_curiosity" or "kmeans_score".
+        :param num_clusters: (int): number of clusters.
         """
         assert (parallel_agents == num_environments) # TODO: make parallelism more flexible
         assert (float(num_environments / parallel_agents).is_integer())
@@ -79,14 +94,21 @@ class ExperimentingSimulatorModel(object):
         self.parallel_agents = parallel_agents
         self.num_environments = num_environments
         self.use_z_only = use_z_only
+        self.metric = metric
+        if self.metric == "softdtw":
+            self.evaluate_trajectories = self.evaluate_trajectories_softdtw # TODO: decide which one to use. For now use tslearn or softdtw
+        elif self.metric == "euclidian":
+            self.evaluate_trajectories = self.evaluate_trajectories_euclidian
+        self.reward = reward
+        self.num_clusters = num_clusters
         self.envs = SubprocVecEnv(
             [_make_env_func() for i in range(self.num_environments)])
         return
 
 
-    def causal_curiosity(self, D, cluster_memberships, num_clusters=2):
+    def causal_curiosity(self, D, cluster_memberships):
 
-        cluster_indices = [np.where(cluster_memberships == i) for i in range(num_clusters)]
+        cluster_indices = [np.where(cluster_memberships == i) for i in range(self.num_clusters)]
 
         C1 = np.amin(D[cluster_indices[0]][:,cluster_indices[1]])
         C2 = np.amax(D[cluster_indices[0]][:,cluster_indices[0]])
@@ -94,7 +116,7 @@ class ExperimentingSimulatorModel(object):
 
         return C1 - C2 - C3
 
-    def evaluate_trajectories(self, action_sequences):
+    def evaluate_trajectories_softdtw(self, action_sequences):
         num_clusters = 2
         observations = self.simulate_trajectories(action_sequences)
         
@@ -107,26 +129,26 @@ class ExperimentingSimulatorModel(object):
         for i in range(rewards.size):
 
             #Lloyd's algorithm
-            centroid_idc = np.random.choice(self.num_environments, num_clusters, replace=False)
+            centroid_idc = np.random.choice(self.num_environments, self.num_clusters, replace=False)
             centroids = observations[i, centroid_idc].copy()
             cluster_memberships = np.zeros(self.num_environments)
             j = 0
             dist = 0
             previous_dist = 0
             while j < max_inner_iters:
-                distances = np.zeros((self.num_environments,num_clusters))
+                distances = np.zeros((self.num_environments,self.num_clusters))
                 for env in range(self.num_environments):
                     for k in range(centroids.shape[0]):
                         if self.use_z_only:
                             D = SquaredEuclidean(centroids[k,np.newaxis], observations[i, env, np.newaxis])
                         else:
                             D = SquaredEuclidean(centroids[k], observations[i, env])
-                        sdtw = SoftDTW(D, gamma=1.0) # gamma is a regularization parameter
+                        sdtw = SoftDTW(D)
                         distances[env,k] = sdtw.compute()
 
                 previous_dist = dist
                 dist = np.sum(np.amin(distances,axis=1))
-                if dist == previous_dist: # coverged
+                if dist == previous_dist: # converged
                     break
 
                 cluster_memberships = np.argmin(distances, axis=1)
@@ -151,31 +173,51 @@ class ExperimentingSimulatorModel(object):
                         SDTW_distance = SquaredEuclidean(observations[i, env_index_1], observations[i, env_index_2])
                     D[env_index_1][env_index_2] = SoftDTW(SDTW_distance).compute()
 
-
             rewards[i] = self.causal_curiosity(D, cluster_memberships)
 
         return rewards
 
-    def _evaluate_trajectories(self, action_sequences):
+    def evaluate_trajectories_euclidian(self, action_sequences):
         observations = self.simulate_trajectories(action_sequences)
+
+        if self.use_z_only:
+            observations = observations[:,:,:,34]
+
         rewards = np.zeros(action_sequences.shape[0])
-        objective = 'paper' # can be "kmeans_score"
         
         for i in range(rewards.size):
-            kmeans = KMeans(n_clusters=2, random_state=0, n_jobs=-1)
-            predictions = kmeans.fit_predict(observations[i].reshape(self.num_environments, -1))
+            observations_flattened = observations[i].reshape(self.num_environments, -1)
+            kmeans = KMeans(n_clusters=self.num_clusters, random_state=0, n_jobs=-1)
+            predictions = kmeans.fit_predict(observations_flattened)
 
-            if objective == 'kmeans_score':
-                # reward as the kmeans score for each action.
-                rewards[i] = kmeans.score(observations[i].reshape(self.num_environments, -1))
-            elif objective == 'paper':
-                # reward as the objective from Causal Curiosity eq. 
-                D = pairwise_distances(observations[i].reshape(self.num_environments, -1))
-                C1 = np.amax(D[np.where(predictions == 0)][:,np.where(predictions == 0)])
-                C2 = np.amax(D[np.where(predictions == 1)][:,np.where(predictions == 1)])
-                C3 = np.amin(D[np.where(predictions == 0)][:,np.where(predictions == 1)])
-                rewards[i] = C3 - C1 - C3
+            if self.reward == "kmeans_score":
+                rewards[i] = kmeans.score(observations_flattened)
+            elif self.reward == "causal_curiosity":
+                D = pairwise_distances(observations_flattened)
+                rewards[i] = self.causal_curiosity(D, predictions)
+        
+        return rewards
 
+    def evaluate_trajectories_tslearn(self, action_sequences):
+        observations = self.simulate_trajectories(action_sequences)
+
+        if self.use_z_only:
+            observations = observations[:,:,:,34]
+
+        rewards = np.zeros(action_sequences.shape[0])
+        
+        for i in range(rewards.size):
+            kmeans = TimeSeriesKMeans(n_clusters=self.num_clusters, metric=self.metric, max_iter=30,
+                                      max_iter_barycenter=100,
+                                      metric_params={"gamma": 1},
+                                      random_state=None).fit(observations[i])
+            predictions = kmeans.predict(observations[i])
+
+            D = np.zeros([self.num_environments, self.num_environments])
+            for env_index_1 in range(self.num_environments):
+                for env_index_2 in range(self.num_environments):
+                    D[env_index_1][env_index_2] = soft_dtw(observations[i,env_index_1],observations[i,env_index_2])
+            rewards[i] = self.causal_curiosity(D, predictions)
         
         return rewards
 
@@ -200,7 +242,6 @@ class ExperimentingSimulatorModel(object):
                 action = action_sequences[j, k]
                 task_observations, _, _, _ = self.envs.step(action)
                 observations[j, :, k] = task_observations
-        print(observations.shape)
         return observations
 
     def end_sim(self):
